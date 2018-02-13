2e17ebc5ab99c310cb5889462d92fd752c62d290
Yegor Kandakov <yegorkandakov@gmail.com>
2018-02-13 20:03:41 +0200

    login/logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,22 @@
-import {USER_LOGGED_IN} from '../types';
-import api from '../api';
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from "../types";
+import api from "../api";
 
-export const userLoggedIn = (user) => ({
+export const userLoggedIn = user => ({
   type: USER_LOGGED_IN,
   user
-})
+});
 
-export const login = (credentials) => (dispatch) =>
-  api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
+export const userLoggedOut = () => ({
+  type: USER_LOGGED_OUT
+});
+
+export const login = credentials => dispatch =>
+  api.user.login(credentials).then(user => {
+    localStorage.bookTrackerJWT = user.token;
+    dispatch(userLoggedIn(user));
+  });
+
+export const logout = () => dispatch =>{
+    localStorage.removeItem('bookTrackerJWT');
+    dispatch(userLoggedOut());
+  };
