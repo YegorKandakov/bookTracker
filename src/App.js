93824212946598c14622e6fbd2e0cbc6dad02e13
Yegor Kandakov <yegorkandakov@gmail.com>
2018-02-25 18:51:33 +0200

    forgot password

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import SignupPage from "./components/pages/SignupPage";
 import LoginPage from "./components/pages/LoginPage";
+import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
+import ResetPasswordPage from "./components/pages/ResetPasswordPage";
 import ConfirmationPage from "./components/pages/ConfirmationPage";
 import DashboardPage from "./components/pages/DashboardPage";
 import UserRoute from './components/routes/UserRoute';
@@ -15,6 +17,8 @@ const App = ({location}) => (
     <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
     <GuestRoute location={location} path="/login" exact component={LoginPage} />
     <GuestRoute location={location} path="/signup" exact component={SignupPage} />
+    <GuestRoute location={location} path="/forgot_password" exact component={ForgotPasswordPage} />
+    <GuestRoute location={location} path="/reset_password/:token" exact component={ResetPasswordPage} />
     <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
   </div>
 );
