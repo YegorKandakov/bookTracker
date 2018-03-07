19eab9173d58b6daaa09ff1abfcacbc723f43e6c
Yegor Kandakov <yegorkandakov@gmail.com>
2018-03-07 18:03:05 +0200

    list and create books

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -4,16 +4,27 @@ import {connect} from 'react-redux';
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
 import {allBooksSelector} from '../../reducers/books';
 import AddBookCta from '../ctas/AddBookCta';
+import { fetchBooks } from "../../actions/books";
 
-const DashboardPage = ({isConfirmed, books}) => (
-  <div>
-    {!isConfirmed && <ConfirmEmailMessage />}
-    {books.length === 0 && <AddBookCta />}
-  </div>
-);
+class DashboardPage extends React.Component {
+  componentDidMount = () => this.onInit(this.props);
+  onInit = props => props.fetchBooks();
+
+  render() {
+    const { isConfirmed, books } = this.props;
+    return (
+      <div>
+        {!isConfirmed && <ConfirmEmailMessage />}
+
+        {books.length === 0 ? <AddBookCta /> : <p>You have books!</p>}
+      </div>
+    );
+  }
+}
 
 DashboardPage.propTypes = {
   isConfirmed: PropTypes.bool.isRequired,
+  fetchBooks: PropTypes.func.isRequired, // eslint-disable-line
   books: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired
   }).isRequired).isRequired
@@ -26,4 +37,5 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(DashboardPage);
+
+export default connect(mapStateToProps, {fetchBooks})(DashboardPage);
